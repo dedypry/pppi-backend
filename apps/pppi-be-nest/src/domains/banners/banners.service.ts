1f2b54bad60e24d736d2679a9805537245bdb619
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2026-06-13 18:52:41 +0700

    update

--- apps/pppi-be-nest/src/domains/banners/banners.service.ts
+++ apps/pppi-be-nest/src/domains/banners/banners.service.ts
@@ -14,6 +14,7 @@ import {
 export class BannersService {
   async list() {
     const banners = await BannerModel.query()
+      .where('is_active', true)
       .withGraphFetched('file')
       .orderBy('id', 'DESC');
 
